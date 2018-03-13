08fa9e4c49ddbc9ca521ced3d3f920ea73a40ab0
Raghu Betina <raghu@firstdraft.com>
2018-03-13 02:13:16 +0000

    Whitelist columns in Doctor for ActiveAdmin

--- app/admin/doctors.rb
+++ app/admin/doctors.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Doctor do
+
+ permit_params :first_name, :last_name, :phone_number, :doctor_image
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
