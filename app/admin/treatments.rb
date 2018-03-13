e03df89c24c5f82356d6751eb5e1fa64ee80e518
Raghu Betina <raghu@firstdraft.com>
2018-03-13 02:13:26 +0000

    Whitelist columns in Treatment for ActiveAdmin

--- app/admin/treatments.rb
+++ app/admin/treatments.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Treatment do
+
+ permit_params :description, :treatment_code
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
