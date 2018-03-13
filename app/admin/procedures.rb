671131a8d6e737b6abd1e94fb60c31efc83324db
Raghu Betina <raghu@firstdraft.com>
2018-03-13 02:13:29 +0000

    Whitelist columns in Procedure for ActiveAdmin

--- app/admin/procedures.rb
+++ app/admin/procedures.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Procedure do
+
+ permit_params :provider_id, :treatment_id, :patient_id, :amount, :date
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
