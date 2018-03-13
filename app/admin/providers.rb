2fa54bf76b5a3767b661dd4e27aa657f293c8f43
Raghu Betina <raghu@firstdraft.com>
2018-03-13 02:13:22 +0000

    Whitelist columns in Provider for ActiveAdmin

--- app/admin/providers.rb
+++ app/admin/providers.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Provider do
+
+ permit_params :doctor_id, :provider_code_string, :location_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
